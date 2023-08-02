00750c90a8ed3e273c112e07848389fb302d2ce0
Rickard Zachrisson <rizach8@gmail.com>
2023-08-02 16:50:09 +0200

    Preserving filter options for inventory

--- Mods/Inventory.cs
+++ Mods/Inventory.cs
@@ -4,9 +4,31 @@ namespace RobOne.Inventory.Mods;
 
 internal class Inventory : ModBase
 {
-    [HarmonyPostfix, HarmonyPatch(typeof(MapDisplay), nameof(MapDisplay.FetchMap))]
-    private static void Banana_Test_fun(MapDisplay __instance)
+    // Block this as it will reset any filter previously set in the inventory
+    [HarmonyPrefix, HarmonyPatch(typeof(InventorySectionDisplay), nameof(InventorySectionDisplay.ResetDefaultSelection))]
+    private static bool InventorySectionDisplay_ResetDefaultSelection_Prefix(InventorySectionDisplay __instance)
+        => false;
+
+    private static int selectedFilter = 15;
+    private static bool openedInventory = false;
+    [HarmonyPrefix, HarmonyPatch(typeof(InventoryMenu), nameof(InventoryMenu.OnSectionSelected), new[] { typeof(int) })]
+    private static void InventoryMenu_OnSectionSelected_Prefix(InventoryMenu __instance, ref int _sectionID)
     {
-        Log.Debug("SHOWING MAP");
+        if (openedInventory)
+        {
+            _sectionID = selectedFilter;
+        }
+        else
+        {
+            selectedFilter = _sectionID;
+        }
+
+        openedInventory = false;
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(InventoryMenu), nameof(InventoryMenu.Show))]
+    private static void InventoryMenu_Show(InventoryMenu __instance)
+    {
+        openedInventory = true;
     }
 }
